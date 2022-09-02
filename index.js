c89a3d9b3cdb5d43d0f7309c5acf5dbff9bd673b
unknown <lopez.thinktank@gmail.com>
2022-09-01 23:06:08 -0700

    updated gitignore file and made sure to include the packages

---
+++ index.js
@@ -0,0 +1,20 @@
+//Packages below are Inquirer and fs, used to create question prompts and store files in memory. 
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the Title of your Project?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    console.log(this);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    console.log(this);
+}
+
+// Function call to initialize app
+init();
